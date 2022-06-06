ffd5782d67f96fb366067281cceb5b2bbdc1bd4b
Zane Ferguson <zferguson@verticalrelevance.com>
2022-06-06 12:41:55 -0500

    archive for TF templates

---
+++ templates/org_master/main.tf
@@ -0,0 +1,39 @@
+
+# Create Organization Security Hub admin account
+resource "aws_organizations_account" "sh_admin_account" {
+  name  = var.sh_admin_account_name 
+  email = var.sh_admin_account_email 
+}
+
+# Make the sh_admin_account_id account the SH admin account
+resource "aws_organizations_delegated_administrator" "security_hub_delegation" {
+  account_id        = aws_organizations_account.sh_admin_account.id
+  service_principal = "securityhub.amazonaws.com"
+}
+
+# Enable Security Hub to be managed at the Org level
+resource "aws_organizations_organization" "org_sh" {
+  aws_service_access_principals = ["securityhub.amazonaws.com"]
+  feature_set                   = "ALL"
+}
+
+# Enables security hub in the org main account
+resource "aws_securityhub_account" "sh_admin_account" {}
+
+# Security Hub Organization level management must be enabled before 
+# assigning a admin account for SH
+resource "aws_securityhub_organization_admin_account" "example" {
+  depends_on = [aws_organizations_organization.org_sh]
+
+  admin_account_id = aws_organizations_account.sh_admin_account.id
+}
+
+# Auto enable security hub in organization member accounts
+resource "aws_securityhub_organization_configuration" "autoenable_sh" {
+  auto_enable = true
+}
+
+
+
+
+
---
+++ templates/org_master/variables.tf
@@ -0,0 +1,8 @@
+variable "sh_admin_account_name" {
+  description = "Name to use for organization SH admin account"
+
+}
+
+variable "sh_admin_account_email" {
+  description = "Email address for SH admin account"
+}
