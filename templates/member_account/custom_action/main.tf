27d534482783a63d7176e1131d6ae1544d4f2431
Zane Ferguson <zferguson@verticalrelevance.com>
2022-06-27 11:17:51 -0500

    update org_master main.tf

---
+++ templates/member_account/custom_action/main.tf
@@ -0,0 +1,71 @@
+
+locals {
+  action_name          = "remove0rules"
+  lambda_name          = "sg_rule_removal"
+  lambda_zip_filename  = "${path.module}/${random_id.lambda_zip_randomizer.keepers.lambda_zip}"
+  lambda_timeout       = 60
+}
+
+resource "random_id" "lambda_zip_randomizer" {
+  keepers = {
+    lambda_zip = "sgr.zip"
+  }
+  byte_length = 12
+}
+
+resource "aws_securityhub_action_target" "remove_action_target" {
+  name        = local.action_name
+  identifier  = local.action_name
+  description = "Custom action to remove 0.0.0.0/0 rules from a security group"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name                = "${local.lambda_name}_role"
+  assume_role_policy  = data.aws_iam_policy_document.lambda_assume_policy.json 
+  inline_policy {
+    name   = "sg_remove_base"
+    policy = data.aws_iam_policy_document.lambda_policy.json
+  }  
+}
+
+resource "aws_lambda_function" "sg_rule_lambda" {
+  function_name = local.lambda_name
+  filename      = local.lambda_zip_filename
+  source_code_hash = filebase64sha256(local.lambda_zip_filename)
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "sgr.lambda_handler"
+  runtime = "python3.9"
+  timeout = local.lambda_timeout
+  environment {
+    variables = {
+      DEBUG = "False"
+    }
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "sg_lambda_rule" {
+  name        = local.action_name
+  description = "Remove 0/0 rules from security groups"
+
+  event_pattern = <<EOF
+{
+  "source": ["aws.securityhub"],
+  "detail-type": ["Security Hub Findings - Custom Action"],
+  "resources": ["${aws_securityhub_action_target.remove_action_target.arn}"]
+}
+EOF
+}
+
+resource "aws_cloudwatch_event_target" "sg_lambda_target" {
+  rule      = aws_cloudwatch_event_rule.sg_lambda_rule.name
+  target_id = local.action_name
+  arn       = aws_lambda_function.sg_rule_lambda.arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "allow_eventbridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.sg_rule_lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.sg_lambda_rule.arn
+}
--- templates/org_master/main.tf
+++ templates/org_master/main.tf
@@ -4,37 +4,68 @@
 # part of the organization initial configuration.
 
 
-# Make the sh_admin_account_id account the SH admin account
+# Make the sh_admin_account_id account a delegated admin for security hub
 resource "aws_organizations_delegated_administrator" "security_hub_delegation" {
   account_id        = var.sh_admin_account_id
   service_principal = "securityhub.amazonaws.com"
   provider = aws.us-east-1
 }
 
-# Sent SH admin account as the admin for security hub in us-east-1
+# Set SH admin account as the admin for security hub in us-east-1
 resource "aws_securityhub_organization_admin_account" "sh_admin_east_1_enable" {
   depends_on = [aws_organizations_delegated_administrator.security_hub_delegation]
   admin_account_id = var.sh_admin_account_id
   provider = aws.us-east-1
 }
 
-# Sent SH admin account as the admin for security hub in us-east-2
+# Set SH admin account as the admin for security hub in us-east-2
 resource "aws_securityhub_organization_admin_account" "sh_admin_east_2_enable" {
   depends_on = [aws_securityhub_organization_admin_account.sh_admin_east_1_enable]
   admin_account_id = var.sh_admin_account_id
   provider = aws.us-east-2
 }
 
-# Sent SH admin account as the admin for security hub in us-west-1
+# Set SH admin account as the admin for security hub in us-west-1
 resource "aws_securityhub_organization_admin_account" "sh_admin_west_1_enable" {
   depends_on = [aws_securityhub_organization_admin_account.sh_admin_east_2_enable]
   admin_account_id = var.sh_admin_account_id
   provider = aws.us-west-1
 }
 
-# Sent SH admin account as the admin for security hub in us-west-2
+# Set SH admin account as the admin for security hub in us-west-2
 resource "aws_securityhub_organization_admin_account" "sh_admin_west_2_enable" {
   depends_on = [aws_securityhub_organization_admin_account.sh_admin_west_1_enable]
+  admin_account_id = var.sh_admin_account_id
+  provider = aws.us-west-2
+}
+
+# Make the sh_admin_account_id account a deletaged admin for guardduty
+resource "aws_organizations_delegated_administrator" "guardduty_delegation" {
+  account_id        = var.sh_admin_account_id
+  service_principal = "guardduty.amazonaws.com"
+  provider = aws.us-east-1
+}
+
+resource "aws_guardduty_organization_admin_account" "gd_admin_us_east_1_enable" {
+  depends_on = [aws_organizations_delegated_administrator.guardduty_delegation]
+  admin_account_id = var.sh_admin_account_id
+  provider = aws.us-east-1
+}
+
+resource "aws_guardduty_organization_admin_account" "gd_admin_us_east_2_enable" {
+  depends_on = [aws_guardduty_organization_admin_account.gd_admin_us_east_1_enable]
+  admin_account_id = var.sh_admin_account_id
+  provider = aws.us-east-2
+}
+
+resource "aws_guardduty_organization_admin_account" "gd_admin_us_west_1_enable" {
+  depends_on = [aws_guardduty_organization_admin_account.gd_admin_us_east_2_enable]
+  admin_account_id = var.sh_admin_account_id
+  provider = aws.us-west-1
+}
+
+resource "aws_guardduty_organization_admin_account" "gd_admin_us_west_2_enable" {
+  depends_on = [aws_guardduty_organization_admin_account.gd_admin_us_west_1_enable]
   admin_account_id = var.sh_admin_account_id
   provider = aws.us-west-2
 }
